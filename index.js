2b3fa515558dc05a6555dca0b54572f6f9952e44
MACBOOK AIR <macbookair@MACBOOKs-MacBook-Air.local>
2023-02-08 18:19:11 +0545

    swqagger

--- index.js
+++ index.js
@@ -3,12 +3,29 @@ const express = require("express");
 const bcrypt=require('bcryptjs')
 // Initialize Express
 const app = express();
-
+const swaggerJsDoc=require('swagger-jsdoc');
+const swaggerUiExpress=require('swagger-ui-express');
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
-
+// swagger setup
+const swaggerOptions={
+  definition:{
+     openapi: '3.0.0',
+     info:{
+         title:"ABHYAM ROBOTICS INSTITUTION OF BUSSINESS AND TECHNOLOGY-Student Management System",
+         description:"Student Management System",
+         contact:{
+             name:'aribt Developers'
+         },
+         servers:[process.env.PORT]
+     }
+  },
+  apis:["./Routes/*.js"]
+ }
+const swaggerDocs=swaggerJsDoc(swaggerOptions);
+app.use('/doc',swaggerUiExpress.serve,swaggerUiExpress.setup(swaggerDocs));
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
